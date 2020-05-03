a88f4cddfe96054cb2cc4cd3d7f1ce8843051183
Bayron <bayron.m.1999@gmail.com>
2020-05-03 18:26:17 -0400

    Crear un metodo menu para alojar la informacion que va a desplegar nuestro menu de la calculadora que esta hecho con un System.out.printl(info);

--- src/Programacion/Menu.java
+++ src/Programacion/Menu.java
@@ -1,5 +1,21 @@
 package Programacion;
 
 public class Menu {
-   
+    //Crear un objeto de la clase que contiene metodos
+
+    //Método que muestra el menú
+
+    public void menu(){
+        //Crear variable para usar el switch case validado con el metodo de validarInt
+        int seleccion;
+
+        //Menú selección
+        System.out.println("Bienvenido a la calculadora, tenemos 7 opciones a elegir\n" +
+                 "1.-Suma\n2.-Resta\n3.-Multiplicación\n" +
+                "4.-División\n5.-Mayor de 2 números\n6.-Calcular potencia de un número\n" +
+                "7.-Salir de la calculadora");
+        //Poner seleccion mas validacion de int
+
+        //switch case para menu
+    }
 }
