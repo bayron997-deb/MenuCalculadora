8f9760b6a963c32b48bd7cda19d177e0ed285497
Bayron <bayron.m.1999@gmail.com>
2020-05-03 18:43:12 -0400

    Commit con bastante informacion: 1-Se agrego 2 metodos nuevos, uno permite decorar y el otro metodo permite hacer el caso 7 de la calculadora

--- src/Programacion/Menu.java
+++ src/Programacion/Menu.java
@@ -7,7 +7,7 @@ public class Menu {
 
     public void menu(){
         //Crear variable para usar el switch case validado con el metodo de validarInt
-        int seleccion;
+        // int seleccion;
 
         //Menú selección
         System.out.println("Bienvenido a la calculadora, tenemos 7 opciones a elegir\n" +
@@ -17,5 +17,62 @@ public class Menu {
         //Poner seleccion mas validacion de int
 
         //switch case para menu
+        switch (seleccion){
+            //ingresaare los metodos cuando los compañeros tengas lista su parte
+            case 1:
+                System.out.println("Usted ha seleccionado suma");
+                break;
+            case 2:
+                System.out.println("Usted ha seleccionado resta");
+                break;
+            case 3:
+                System.out.println("Usted ha seleccionado multiplicación");
+                break;
+            case 4:
+                System.out.println("Usted ha seleccionado división");
+                break;
+            case 5:
+                System.out.println("Usted ha seleccionado elegir el mayor de 2 numeros");
+                break;
+            case 6:
+                System.out.println("Usted ha seleccionado potencia de un numero");
+                break;
+            case 7:
+                break;
+            default:
+                System.out.println("La opción ingresada no existe");
+        }
     }
-}
+    //Decoracion
+    public void linea(){
+        System.out.println("------------------------------------------------");
+    }
+    public void salir() {
+        boolean valido = false;
+        int eleccion;
+        do {
+            System.out.println("¿Desea salir de la calculadora? 1=Sí 0=No");
+            //agregar eleccion con su metodo en operaciones
+            eleccion = ope.validarInt();
+
+            switch (eleccion) {
+                case 1:
+                    Calculadora.seguir = false;//se debe agregar una clase que contenga metodo seguir
+                    valido = true;
+                    break;
+                case 0:
+                    valido = true;
+                    break;
+                default:
+                    System.out.println("La opción ingresada no existe");
+            }
+        } while(!valido);
+    }
+
+
+
+
+        }
+
+
+
