6fc6ee143faff6c37a317e6a64d128921e9e86ca
Bayron <bayron.m.1999@gmail.com>
2020-05-06 13:24:26 -0400

    Se agrego una clase main para desplegar el menu, se dejo en comentario el metodo salir porque contenia el llamado de la clase operaciones y daba error. con esta parte se muestra el menu y la seleccion(no validada porque es un metodo que no me corresponde)

---
+++ src/Programacion/Main.java
@@ -0,0 +1,13 @@
+package Programacion;
+
+import java.util.Scanner;
+
+public class Main {
+   public static Scanner in = new Scanner(System.in);
+    public static void main(String arg[]){
+        Menu menu = new Menu();
+
+        menu.menu();
+
+    }
+}
--- src/Programacion/Menu.java
+++ src/Programacion/Menu.java
@@ -5,48 +5,59 @@ public class Menu {
 
     //Método que muestra el menú
 
-    public void menu(){
+    public void menu() {
         //Crear variable para usar el switch case validado con el metodo de validarInt
         // int seleccion;
-
+        int seleccion;
         //Menú selección
         System.out.println("Bienvenido a la calculadora, tenemos 7 opciones a elegir\n" +
-                 "1.-Suma\n2.-Resta\n3.-Multiplicación\n" +
+                "1.-Suma\n2.-Resta\n3.-Multiplicación\n" +
                 "4.-División\n5.-Mayor de 2 números\n6.-Calcular potencia de un número\n" +
                 "7.-Salir de la calculadora");
-        //Poner seleccion mas validacion de int
+        //Se crea una Seleccion no validada
+        seleccion = Main.in.nextInt();
 
         //switch case para menu
-        switch (seleccion){
+        switch (seleccion) {
             //ingresaare los metodos cuando los compañeros tengas lista su parte
             case 1:
                 System.out.println("Usted ha seleccionado suma");
+                linea();
                 break;
             case 2:
                 System.out.println("Usted ha seleccionado resta");
+                linea();
                 break;
             case 3:
                 System.out.println("Usted ha seleccionado multiplicación");
+                linea();
                 break;
             case 4:
                 System.out.println("Usted ha seleccionado división");
+                linea();
                 break;
             case 5:
                 System.out.println("Usted ha seleccionado elegir el mayor de 2 numeros");
+                linea();
                 break;
             case 6:
                 System.out.println("Usted ha seleccionado potencia de un numero");
+                linea();
                 break;
             case 7:
+                //salir();
+                linea();
                 break;
             default:
                 System.out.println("La opción ingresada no existe");
         }
     }
+
     //Decoracion
-    public void linea(){
+    public void linea() {
         System.out.println("------------------------------------------------");
     }
+    /*
     public void salir() {
         boolean valido = false;
         int eleccion;
@@ -67,12 +78,14 @@ public class Menu {
                     System.out.println("La opción ingresada no existe");
             }
         } while(!valido);
-    }
+     */
+}
 
 
 
 
-        }
+
+
 
 
 
